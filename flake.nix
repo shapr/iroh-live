1d33f2012a61fe9bdc46d91a9bb9ed32b3bc08e9
Shae Erisson <shae+jj@scannedinavian.com>
2026-02-12 19:20:13 -0500

    nixos build but rust-lld fails to link

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "magic-cap is a command line utility for an always encrypted archive file type.";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = { url = "github:oxalica/rust-overlay"; };
+  };
+  outputs = { nixpkgs, nixpkgs-unstable, rust-overlay, ... }:
+    let
+      system = "x86_64-linux";
+    in {
+      packages.${system}.default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in pkgs.rustPlatform.buildRustPackage {
+          pname = "magic_cap";
+          buildInputs = [ ];
+          version = "0.1.0";
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+        };
+      devShells.${system}.default =
+        let pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ (import rust-overlay) ];
+              config.allowUnfree = true;
+            };
+            upkgs = import nixpkgs-unstable { inherit system; };
+        in
+          pkgs.mkShell {
+            libraries = with pkgs; [ wayland-utils waylandpp ];
+            packages = with pkgs; [ rust-bin.stable.latest.default upkgs.rust-analyzer cargo cargo-make
+                                    pkg-config clippy ffmpeg-full wayland wayland-protocols
+                                    wayland-scanner pipewire llvmPackages.clangUseLLVM alsa-lib
+                                    egl-wayland pkgs.rustPlatform.bindgenHook libGL libtool automake
+                                    autoconf
+                                  ];
+          };
+    };
+}
